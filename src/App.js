0b91f649bfa09298bc2b27e4db579514e5db6bca
Emmanuel Orozco <indi3.rok@gmail.com>
2022-01-17 16:09:29 +0100

    added project update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("https://myapp-123354.herokuapp.com/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setMsg(json.message);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{msg}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
